d341d22b92ddd7b7ee59ea8b8b9afbd0ce5c262b
Sayan Mondal <analystsayan1@gmail.com>
2025-08-13 21:08:55 +0530

    the whole new exprience is here.

--- js/script.js
+++ js/script.js
@@ -8,3 +8,23 @@ hamburger.addEventListener('click', () => {
         navLinks.style.display = 'flex';
     }
 });
+
+
+
+// batch card
+document.addEventListener("DOMContentLoaded", () => {
+    const cards = document.querySelectorAll(".latest-batch-card");
+
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                cards.forEach(c => c.classList.remove("active"));
+                entry.target.classList.add("active");
+            }
+        });
+    }, {
+        threshold: .6 // 60% visible means it's in the center
+    });
+
+    cards.forEach(card => observer.observe(card));
+});
