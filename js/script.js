d55eae574b2cb10bb27cd3997c00ccb4f19f048c
Sayan Mondal <analystsayan1@gmail.com>
2025-08-19 18:25:14 +0530

    quiz section updation with 2 english quiz

--- js/script.js
+++ js/script.js
@@ -211,4 +211,19 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 
+// free quiz
+
+document.querySelectorAll(".accordion-header").forEach(header => {
+    header.addEventListener("click", () => {
+        const item = header.parentElement;
+        item.classList.toggle("active");
+
+        // Close others (optional: accordion effect)
+        document.querySelectorAll(".accordion-item").forEach(other => {
+            if (other !== item) other.classList.remove("active");
+        });
+    });
+});
+
+
 
